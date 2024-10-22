7f72287b6cc6a22944cdd8333b17a0151bec41f3
firozshekh <firozshekh377@gmail.com>
2024-10-22 22:37:49 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Public from "./routes/Public";
 import Home from "./pages/Home";
+import Shop from "./pages/shop";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Public element={<Home />} />} />
+      <Route path="/shop" element={<Public element={<Shop />} />} />
       <Route path="/about" element="" />
     </Routes>
   );
---
+++ src/pages/shop/index.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import PopularProducts from '../../components/PopularProducts'
+
+const Shop = () => {
+  return (
+    <div>AboutpCart
+     
+    </div>
+  )
+}
+
+export default Shop
+

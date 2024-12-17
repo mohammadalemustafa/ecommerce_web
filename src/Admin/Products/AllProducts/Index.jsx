1b5ff4329e6d4df041d89fb8f5339620e7d1ee63
ranjeetkumarui6 <ranjeetkumarui93@gmail.com>
2024-12-17 23:51:28 +0530

    product table is added

--- src/Admin/Products/AllProducts/Index.jsx
+++ src/Admin/Products/AllProducts/Index.jsx
@@ -1,32 +1,107 @@
-import React from 'react'
-import styles from './index.module.css'
-import { FiPlus } from 'react-icons/fi'
+import React from "react";
+import styles from "./index.module.css";
+import { FiPlus } from "react-icons/fi";
+import { FaEdit, FaTrash } from "react-icons/fa";
+import Fiels from "../AddProducts/General/Fields/Index";
+import { Allproducts } from "../../../data/AllProduct";
 const AllProducts = () => {
   return (
     <>
       <div className={styles.allproductscontainer}>
         <div className={styles.allproducts}>
-            <div className={styles.allproducthead}>
-                <h2>Products</h2>
-                <div className={styles.addallproduct}>
-                    <i><FiPlus size={20}/></i>
-                    <span>Add Product</span>
-                </div>
+          <div className={styles.allproducthead}>
+            <h2>Products</h2>
+            <div className={styles.addallproduct}>
+              <i>
+                <FiPlus size={20} />
+              </i>
+              <span>Add Product</span>
             </div>
-            <div className={styles.allproducthead}>
-                <div className={styles.items}>
-                    <span>Items:</span>
-                    <span>78</span>
-                    <span>Items per page</span>
-                </div>
-                <div className={styles.search}>
-                    <input type="search" placeholder='search here....'/>
-                </div>
+          </div>
+          <div className={styles.allproducthead}>
+            <div className={styles.items}>
+              <span>Items:</span>
+              <span>78</span>
+              <span>Items per page</span>
             </div>
+            <div className={styles.search}>
+              <input type="search" placeholder="search here...." />
+            </div>
+          </div>
+          <div className={styles.allproducttablecontainer}>
+            <table>
+              <thead>
+                <tr>
+                  <th>S.No.</th>
+                  <th>Image</th>
+                  <th>Name</th>
+                  <th>SKU</th>
+                  <th>Price</th>
+                  <th>Stock</th>
+                  <th>Store</th>
+                  <th>Approve</th>
+                  <th>Status</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+            </table>
+            <tbody>
+              
+              {
+                Allproducts.map(it=>{
+                  return <tr key={it.id}>
+                  <td>{it.id}</td>
+                  <td>
+                    <img src={it.img} style={{width:"50px", height:"50px"}} alt="topsellpic" />
+                  </td>
+                  <td>{it.name}</td>
+                  <td>{it.sku}</td>
+                  <td>${it.price}</td>
+                  <td>
+                  <span>{it.stock}</span>
+                  </td>
+                  <td>{it.store}</td>
+                  <td>
+                    <div className={styles.toggleborder}>
+                      <input id="one" type="checkbox" />
+                      <label htmlFor="one">
+                        <div
+                          className={styles.handle}
+                         
+                        ></div>
+                      </label>
+                    </div>
+                  </td>
+                  <td>
+                    <div className={styles.toggleborder}>
+                      <input id="one" type="checkbox" />
+                      <label htmlFor="one">
+                        <div
+                          className={styles.handle}
+                          
+                        ></div>
+                      </label>
+                    </div>
+                  </td>
+                  <td>
+                    <i>
+                      <FaEdit color="blue" size={20} />
+                    </i>
+                    &nbsp;&nbsp;&nbsp;
+                    <i>
+                      <FaTrash color="red" size={20} />
+                    </i>
+                  </td>
+                </tr>
+                })
+              }
+              
+            </tbody>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default AllProducts
+export default AllProducts;
---
+++ src/data/AllProduct.js
@@ -0,0 +1,115 @@
+import pic1 from '../assets/top_sell/img1.jpg'
+import pic2 from '../assets/top_sell/mig2.jpg'
+import pic3 from '../assets/top_sell/img3.jpg'
+import pic4 from '../assets/top_sell/img4.jpg'
+import pic5 from '../assets/top_sell/img5.jpg'
+import pic6 from '../assets/top_sell/img6.jpg'
+import pic7 from '../assets/top_sell/img7.jpg'
+import pic8 from '../assets/top_sell/img8.jpg'
+import pic9 from '../assets/top_sell/img9.jpg'
+import pic10 from '../assets/top_sell/img10.jpg'
+import pic11 from '../assets/top_sell/img11.jpg'
+import pic12 from '../assets/top_sell/img12.jpg'
+
+
+export const Allproducts=[
+    {
+        id:1,
+        img:pic1,
+        name:"Gourmet Fresh Pomegranate",
+        sku:"FRUIT10",
+        price:"6.78",
+        stock:"In Stock",
+        store:"Fruits Market"
+    },
+    {
+        id:2,
+        img:pic2,
+        name:"Gourmet Fresh Pomegranate",
+        sku:"FRUIT10",
+        price:"6.78",
+        stock:"In Stock",
+        store:"Fruits Market"
+    },
+    {
+        id:3,
+        img:pic3,
+        name:"Gourmet Fresh Pomegranate",
+        sku:"FRUIT10",
+        price:"6.78",
+        stock:"In Stock",
+        store:"Fruits Market"
+    },{
+        id:4,
+        img:pic4,
+        name:"Gourmet Fresh Pomegranate",
+        sku:"FRUIT10",
+        price:"6.78",
+        stock:"In Stock",
+        store:"Fruits Market"
+    },{
+        id:5,
+        img:pic5,
+        name:"Gourmet Fresh Pomegranate",
+        sku:"FRUIT10",
+        price:"6.78",
+        stock:"In Stock",
+        store:"Fruits Market"
+    },{
+        id:6,
+        img:pic6,
+        name:"Gourmet Fresh Pomegranate",
+        sku:"FRUIT10",
+        price:"6.78",
+        stock:"In Stock",
+        store:"Fruits Market"
+    },{
+        id:7,
+        img:pic7,
+        name:"Gourmet Fresh Pomegranate",
+        sku:"FRUIT10",
+        price:"6.78",
+        stock:"In Stock",
+        store:"Fruits Market"
+    },{
+        id:8,
+        img:pic8,
+        name:"Gourmet Fresh Pomegranate",
+        sku:"FRUIT10",
+        price:"6.78",
+        stock:"In Stock",
+        store:"Fruits Market"
+    },{
+        id:9,
+        img:pic9,
+        name:"Gourmet Fresh Pomegranate",
+        sku:"FRUIT10",
+        price:"6.78",
+        stock:"In Stock",
+        store:"Fruits Market"
+    },{
+        id:10,
+        img:pic10,
+        name:"Gourmet Fresh Pomegranate",
+        sku:"FRUIT10",
+        price:"6.78",
+        stock:"In Stock",
+        store:"Fruits Market"
+    },{
+        id:11,
+        img:pic11,
+        name:"Gourmet Fresh Pomegranate",
+        sku:"FRUIT10",
+        price:"6.78",
+        stock:"In Stock",
+        store:"Fruits Market"
+    },{
+        id:12,
+        img:pic12,
+        name:"Gourmet Fresh Pomegranate",
+        sku:"FRUIT10",
+        price:"6.78",
+        stock:"In Stock",
+        store:"Fruits Market"
+    },
+]
